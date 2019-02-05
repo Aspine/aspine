184d0de13ddc8803fc238f15c06ecf28cecf0db8
maxtkc <maxkatzchristy@gmail.com>
2019-02-04 21:31:31 -0500

    added basic server

---
+++ serve.js
@@ -0,0 +1,25 @@
+#!/bin/node
+
+// --------------- Includes ------------------
+const express = require('express');
+const scraper = require('./scrape.js');
+const bodyParser = require('body-parser');
+
+// -------------------------------------------
+
+
+// ------------ Web Server -------------------
+const app = express();
+const port = 8080;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/data', async (req, res) => {
+    console.log(`\n\nNEW LOGIN: ${req.body.username}\n------------------`);
+    res.send(await scraper.scrape_student(req.body.username, req.body.password));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// -------------------------------------------
