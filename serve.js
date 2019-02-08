671ad9834812b15ea573dc85b1db60694d023bb2
maxtkc <maxkatzchristy@gmail.com>
2019-02-08 12:05:35 -0500

    added socketio files

--- serve.js
+++ serve.js
@@ -26,13 +26,16 @@ app.use(session({ // Allows for sessions, and signs them with the (arbitrary) se
 
 app.post('/data', async (req, res) => {
     console.log(`\n\nNEW LOGIN: ${req.session.username}\n------------------`);
-    // USE REAL DATA:
-    let capturingData = await scraper.scrape_student(req.session.username, req.session.password);
+
+    //let capturingData = await scraper.scrape_student(req.session.username, req.session.password);
     //console.log(JSON.parse(capturingData)); was trying to get output sample data but couldn't get it to work and had to go
-    res.send(capturingData);
+    //res.send(capturingData);
+
+    // USE REAL DATA:
+    //await scraper.scrape_student(req.session.username, req.session.password);
 
     // USE FAKE DATA:
-    //res.sendFile('sample.json', {root:"public"});
+    res.sendFile('sample.json', {root:"public"});
 });
 app.post('/login', async (req, res) => {
     req.session.username = req.body.username;
