0b8478845d60229aeaa0906fe2d5d3700a41d765
Gautier Killian <g.cole.killian@gmail.com>
2019-02-11 08:04:29 -0500

    changed gpa to 100 point scale

--- public/calculate_grade.js
+++ public/calculate_grade.js
@@ -134,11 +134,10 @@ function computeGPA() {
 	let counter = 0.0;
 	for (let i = 0; i < tableData.classes.length; i++) {
 		if (isNaN(tableData.classes[i].grade)) {
-			console.log(parseFloat(tableData.classes[i].grade));
-			sum += getGPA(parseFloat(tableData.classes[i].grade));
+			sum += parseFloat(tableData.classes[i].grade);
 			counter += 1.0;
 		}
 	}
-	return Math.round(sum / counter) + ".0";
+	return Math.round(sum / counter * 100) / 100;
 
 }
