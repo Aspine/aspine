f9ab6dcb50549648d1424d23af9764d287854dfd
Gautier Killian <g.cole.killian@gmail.com>
2019-02-22 23:55:10 -0500

    web manifest implementation complete

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 var cacheName = "v1";
 var cacheFiles = [
+	'/index.html',
 ];
 
 self.addEventListener('install', function(event) {
@@ -30,7 +31,33 @@ self.addEventListener('activate', function(event) {
 	);
 });
 
-self.addEventListener('fetch', function(event) {
-  // Perform install steps
-  //console.log("[ServiceWorker] Fetching", event.request.url);
+//self.addEventListener('fetch', function(event) {
+//  // Perform install steps
+//  //console.log("[ServiceWorker] Fetching", event.request.url);
+//});
+
+self.addEventListener('fetch', function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                console.log('Found response in cache:', response);
+
+                return response;
+            }
+
+            console.log('No response found in cache. About to fetch from network...');
+
+            return fetch(event.request).then(function (response) {
+                console.log('Response from network is:', response);
+
+                return response;
+            }).catch(function (error) {
+                console.error('Fetching failed:', error);
+
+                return caches.match(OFFLINE_URL);
+            });
+        })
+    );
 });
