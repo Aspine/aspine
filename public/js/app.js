6e24284ee3e0d7e91a824ca9f3f46f53434db463
Gautier Killian <g.cole.killian@gmail.com>
2019-02-23 00:13:07 -0500

    remove console.logssss

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,10 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('./sw.js', { scope: './' }).then(function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      //console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
       // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+      //console.log('ServiceWorker registration failed: ', err);
     });
   });
 }
--- public/sw.js
+++ public/sw.js
@@ -37,24 +37,24 @@ self.addEventListener('activate', function(event) {
 //});
 
 self.addEventListener('fetch', function (event) {
-    console.log('Handling fetch event for', event.request.url);
+    //console.log('Handling fetch event for', event.request.url);
 
     event.respondWith(
         caches.match(event.request).then(function (response) {
             if (response) {
-                console.log('Found response in cache:', response);
+                //console.log('Found response in cache:', response);
 
                 return response;
             }
 
-            console.log('No response found in cache. About to fetch from network...');
+            //console.log('No response found in cache. About to fetch from network...');
 
             return fetch(event.request).then(function (response) {
-                console.log('Response from network is:', response);
+                //console.log('Response from network is:', response);
 
                 return response;
             }).catch(function (error) {
-                console.error('Fetching failed:', error);
+                //console.error('Fetching failed:', error);
 
                 return caches.match(OFFLINE_URL);
             });
