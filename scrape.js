a1429ca3baf2d2386858aa2617b339eb42887076
Gautier Killian <g.cole.killian@gmail.com>
2019-02-28 22:22:34 -0500

    allow for multiple statistic checks

--- scrape.js
+++ scrape.js
@@ -73,6 +73,24 @@ async function scrape_assignmentDetails(session_id, apache_token, assignment_id)
 			"method":"POST",
 			"mode":"cors"}));
 
+	(await fetch_body("https://aspen.cpsd.us/aspen/portalAssignmentList.do?navkey=academics.classes.list.gcd",
+		{"credentials":"include",
+			"headers":{"Connection": "keep-alive",
+				"Upgrade-Insecure-Requests": "1",
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.12.0 Chrome/69.0.3497.128 Safari/537.36",
+				"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+				"X-Do-Not-Track": "1",
+				"Accept-Language": "en-US,en",
+				"DNT": "1",
+				"Referer": "https://aspen.cpsd.us/aspen/portalClassDetail.do?navkey=academics.classes.list.detail",
+				"Accept-Encoding": "gzip, deflate, br",
+				"Cookie": "deploymentId=x2sis; JSESSIONID=" + session_id},
+			"referrer":"https://aspen.cpsd.us/aspen/portalClassDetail.do?navkey=academics.classes.list.detail",
+			"referrerPolicy":"strict-origin-when-cross-origin",
+			"body":null,
+			"method":"GET",
+			"mode":"cors"}));
+
 	let statistics = [];
 	
 	$('td[width="50%"]').eq(1).find('tr').parent().children().each(function(i, elem) {
@@ -331,6 +349,7 @@ async function scrape_assignments(session_id, apache_token) {
 			"body":null,
 			"method":"GET",
 			"mode":"cors"}));
+
 	let data = [];
    let page = 1;
     let n_assignments = parseInt($("#totalRecordsCount").text());
