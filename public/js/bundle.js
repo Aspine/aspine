a629ee87dc7a133439fec1f5a36d9dc2947c2f63
maxtkc <maxkatzchristy@gmail.com>
2019-03-04 19:07:05 -0500

    fixed stats request (hopefully for real)

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -39514,7 +39514,7 @@ window.getStats = async function (session_id, apache_token, assignment_id) {
 		console.log("getting stats");
 		let stats = "before";
 
-		request.post({url:'/stats', form: {session_id: session_id, apache_token: apache_token, assignment_id: assignment_id}}, 
+		request.post({url:'https://aspine.us/stats', form: {session_id: session_id, apache_token: apache_token, assignment_id: assignment_id}}, 
 			
 			function(err, httpResponse, body){
 				console.log("function");
