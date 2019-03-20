f09412ab5f7f219359a5deaa4962840fd4813daa
Gautier Killian <g.cole.killian@gmail.com>
2019-03-19 22:42:38 -0400

    remove so much rainbow

--- public/js/extraFunctions.js
+++ public/js/extraFunctions.js
@@ -157,7 +157,7 @@ let rowGradeFormatter = function(cell, formatterParams) {
 
 	} else {
 		let value = parseFloat(cell.getValue()) + "% " + getLetterGrade(cell.getValue());
-		if (numberGrade >= 100) {
+		if (numberGrade > 100) {
 			return "<span style='background: -webkit-linear-gradient(left, red, orange, green, blue, purple);-webkit-background-clip: text; -webkit-text-fill-color:transparent; font-weight:bold;'>" + value + "</span>";
 		} else {
 			return "<span style='color:" + rowColor + "; font-weight:bold;'>" + value + "</span>";
