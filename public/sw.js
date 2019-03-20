c03b8adf017a1afea8dd00d25fda0d26ff4137f8
Gautier Killian <g.cole.killian@gmail.com>
2019-03-20 18:10:26 -0400

    service worker patch

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-var cacheName = "v2";
+var cacheName = "v3";
 var cacheFiles = [
-	'/',
+	'./',
 	'/login.html',
 	//'/home.html',
 	//'/manifest.json',
@@ -44,33 +44,55 @@ self.addEventListener('activate', function(event) {
 	);
 });
 
-//self.addEventListener('fetch', function(event) {
-//  // Perform install steps
-//  //console.log("[ServiceWorker] Fetching", event.request.url);
-//});
-
-self.addEventListener('fetch', function (event) {
-    //console.log('Handling fetch event for', event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                //console.log('Found response in cache:', response);
-
-                return response;
-            }
-
-            console.log('No response found in cache. About to fetch from network...');
-
-            return fetch(event.request).then(function (response) {
-                console.log('Response from network is:', response);
-
-                return response;
-            }).catch(function (error) {
-                console.error('Fetching failed:', error);
-
-                return caches.match(OFFLINE_URL);
-            });
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  // Perform install steps
+  //console.log("[ServiceWorker] Fetching", event.request.url);
 });
+self.addEventListener('fetch', event => {
+  // Let the browser do its default thing
+  // for non-GET requests.
+  if (event.request.method != 'GET') return;
+
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cache = await caches.open('cacheFiles');
+    const cachedResponse = await cache.match(event.request);
+
+    if (cachedResponse) {
+      // If we found a match in the cache, return it, but also
+      // update the entry in the cache in the background.
+      event.waitUntil(cache.add(event.request));
+      return cachedResponse;
+    }
+
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
+});
+
+//self.addEventListener('fetch', function (event) {
+//    //console.log('Handling fetch event for', event.request.url);
+//
+//    event.respondWith(
+//        caches.match(event.request).then(function (response) {
+//            if (response) {
+//                //console.log('Found response in cache:', response);
+//
+//                return response;
+//            }
+//
+//            console.log('No response found in cache. About to fetch from network...');
+//
+//            return fetch(event.request).then(function (response) {
+//                console.log('Response from network is:', response);
+//
+//                return response;
+//            }).catch(function (error) {
+//                console.error('Fetching failed:', error);
+//
+//                return caches.match(OFFLINE_URL);
+//            });
+//        })
+//    );
+//});
