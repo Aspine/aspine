43567829a6ed8dea7b93f7380fa20d182353f8b9
Gautier Killian <g.cole.killian@gmail.com>
2019-03-26 19:04:55 -0400

    stashing

--- public/sw_cached_site.js
+++ public/sw_cached_site.js
@@ -1,9 +1,23 @@
-const cacheName = 'v2';
+const cacheName = 'v5';
 
+let currentCache = {
+    offline: 'offline-cache' + cacheName
+};
 
+offlineUrl = 'offline.html';
 
 // Call Install Event
 self.addEventListener('install', (e) => {
+
+    e.waitUntil(
+        caches.open(currentCache.offline)
+        .then(cache => {
+            return cache.addAll([
+                offlineUrl
+            ]);
+        })
+    );
+
     console.log("Service Worker: Installed");
 });
 
@@ -16,7 +30,7 @@ self.addEventListener('activate', (e) => {
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
-                    if(cache !== cacheName) {
+                    if(cache !== cacheName && cache !== currentCache.offline) {
                         console.log('Service Worker: Clearing Old Cache');
                         return caches.delete(cache);
                     }
@@ -28,19 +42,28 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
     console.log("Service Worker: Fetching");
-    
-    e.respondWith(
-        fetch(e.request)
-        .then(res => {
-            // Make a copy/clone of response
-            const resClone = res.clone();
-            // Open cache
-            caches
-                .open(cacheName)
-                .then(cache => {
-                    cache.put(e.request, resClone);
-                });
-            return res;
-        }).catch(err => caches.match(e.request).then(res => res))
-    );
+
+    if (e.request.mode === 'navigate' || (e.request.method === 'GET' && e.request.headers.get('accept').includes('text/html'))) {
+        e.respondWith(
+            fetch(e.request.url).catch(error => {
+                // Return the offline page
+                return caches.match(offlineUrl);
+            })
+        );
+    } else {
+        e.respondWith(
+            fetch(e.request)
+            .then(res => {
+                // Make a copy/clone of response
+                const resClone = res.clone();
+                // Open cache
+                caches
+                    .open(cacheName)
+                    .then(cache => {
+                        cache.put(e.request, resClone);
+                    });
+                return res;
+            }).catch(err => caches.match(e.request).then(res => res))
+        );
+    }
 });
