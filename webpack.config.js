62dd43ac97e6e34eb7a777dc51777b83a97ee06a
maxtkc <maxkatzchristy@gmail.com>
2019-03-27 23:40:44 -0400

    Revert to bundling css in bundle.js with side effect of FOUC

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,78 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  //plugins: [
+  //  new HtmlWebpackPlugin({
+  //    template: './src/index.html',
+  //    title: 'Output Management',
+  //    filename: 'index.html'
+  //  })
+  //],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
     rules: [
+      //{
+      //  test: /\.html$/,
+      //  use: [
+      //    "file-loader",
+      //    {
+      //      loader: "extract-loader",
+      //      options: {
+      //        name: "index.html",
+      //      },
+      //    },
+      //    {
+      //      loader: "html-loader",
+      //      options: {
+      //        attrs: ["img:src", "link:href"]
+      //      }
+      //    }
+      //  ]
+      //},
       {
         test: /\.html$/,
         use: [
-          "file-loader",
           {
-            loader: "extract-loader",
+            loader: "file-loader",
             options: {
-              name: "index.html",
-            },
-          },
-          {
-            loader: "html-loader",
-            options: {
-              attrs: ["img:src", "link:href"]
+              name: '[name].[ext]'
             }
           }
         ]
       },
-      //{
-      //  test: /\.css$/,
-      //  //use: ['style-loader', 'css-loader']
-      //  use: ['file-loader', 'extract-loader', 'css-loader']
-      //}
       {
         test: /\.css$/,
-          use: [
-            "file-loader",
-            {
-              loader: "extract-loader",
-              options: {
-                name: "bootstrap.css",
-              },
-            },
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: false
-              }
-            }
-          ]
+        use: ['style-loader', 'css-loader']
       }
+      //{
+      //  test: /\.css$/,
+      //    use: [
+      //      "file-loader",
+      //      {
+      //        loader: "extract-loader",
+      //        options: {
+      //          name: "bootstrap.css",
+      //        },
+      //      },
+      //      {
+      //        loader: "css-loader",
+      //        options: {
+      //          sourceMap: false
+      //        }
+      //      }
+      //    ]
+      //}
     ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
   }
 };
