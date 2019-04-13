aa7a98a7e15f3ef47a6950ef40dce15d26c102bb
Gautier Killian <g.cole.killian@gmail.com>
2019-04-13 18:51:17 -0400

    very pretty dropdown

--- public/js/extraFunctions.js
+++ public/js/extraFunctions.js
@@ -399,3 +399,23 @@ function showPDFDropdown() {
   //  x.className = x.className.replace(" w3-show", "");
   //}
 }
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
