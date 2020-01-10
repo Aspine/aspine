661a50b44c1d676807739e1005748d09254b2d11
Cole Killian <g.cole.killian@gmail.com>
2020-01-09 22:02:48 -0500

    Disable redis in serve.js
    Hopefully we will soon restore the functionality of the calendar, but
    for now it is not active. Therefore there is no need for redis. I just
    commented out the lines involved redis so that it can be restored easily
    at a later point.

--- serve.js
+++ serve.js
@@ -5,9 +5,9 @@ const express = require('express');
 const scraper = require('./scrape.js');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const redis = require("redis"),
-    client = redis.createClient(6310);
+// const RedisStore = require('connect-redis')(session);
+// const redis = require("redis"),
+//     client = redis.createClient(6310);
 const http = require('http');
 const socket = require('socket.io');
 const fs = require('fs');
@@ -41,10 +41,10 @@ const server = app.listen(port,
     () => console.log(`Example app listening on port ${port}!`));
 const io = socket(server);
 
-// redis setup
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
+// // redis setup
+// client.on("error", function (err) {
+//     console.log("Error " + err);
+// });
 
 if(!(args.hasOwnProperty("insecure") || args._.includes("insecure"))) {
     // Certificate
@@ -92,7 +92,7 @@ const options = {
     port: 6310
 }
 app.use(session({
-    store: new RedisStore(options),
+    // store: new RedisStore(options),
     secret: 'scheming+anaconda+bunkbed+greeting+octopus+ultimate+viewable+hangout+everybody',
     resave: false,
     saveUninitialized: false
@@ -207,71 +207,71 @@ app.get('/logout', async (req, res) => {
 	res.redirect('/login.html');
 });
 
-app.post('/set-settings', async (req, res) => {
-    // TODO: Sanitization
-    let key = crypto.createHash('md5').update(req.session.username).digest('hex');
-    client.set(`settings:${key}`, JSON.stringify(req.body));
-    res.status(200).send("set settings");
-});
-
-app.get('/get-settings', async (req, res) => {
-    if(typeof(req.session.username) == "undefined") {
-        res.status(400).send("User not logged in");
-        return;
-    }
-    let key = crypto.createHash('md5').update(req.session.username).digest('hex');
-    client.get(`settings:${key}`, function (err, reply) {
-        if(!reply) {
-            //res.send(JSON.stringify({calendars:[]}));
-            res.send({calendars:["CRLS", "holidays"]});
-        } else {
-            res.send(JSON.parse(reply));
-        }
-    });
-});
-
-app.post('/add-calendar', async (req, res) => {
-    console.log(req.body.color);
-    // Security: MUST SANITIZE URLS
-    if(req.body.id == undefined || !validator.isEmail(req.body.id) ||
-        req.body.name == undefined || !req.body.name.match(/^[\-0-9a-zA-Z.'' ]+$/g) ||
-        req.body.color == undefined || !req.body.color.match(/^([\-0-9a-fA-F]){6}$/g)) {
-        res.status(400).send("Malformed ID, Name, or Color");
-        return;
-    }
-
-    // Check to see if it is public and working
-    const calendar = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(req.body.id)}/events?key=AIzaSyDtbQCoHa4lC4kW4g4YXTyb8f5ayJct2Ao&timeMin=2019-02-23T00%3A00%3A00Z&timeMax=2019-04-08T00%3A00%3A00Z&singleEvents=true&maxResults=9999&_=1552838482460`);
-    if(!calendar.ok) {
-        res.status(400).send("Bad Calendar ID");
-        return;
-    }
-
-    let cal = JSON.stringify({
-        name: req.body.name,
-        id: req.body.id,
-        color: req.body.color
-    });
-    client.lrem('calendars', 0, cal);
-
-    client.rpush('calendars', 
-    JSON.stringify({
-        name: req.body.name,
-        id: req.body.id,
-        color: req.body.color
-    }));
-
-    res.status(200).send("added calendar");
-});
-
-app.get('/get-calendars', async (req, res) => {
-    client.lrange(`calendars`, 0, -1, function (err, reply) {
-        for(i in reply) {
-            reply[i] = JSON.parse(reply[i]);
-        }
-        res.send(reply);
-    });
-});
+// app.post('/set-settings', async (req, res) => {
+//     // TODO: Sanitization
+//     let key = crypto.createHash('md5').update(req.session.username).digest('hex');
+//     client.set(`settings:${key}`, JSON.stringify(req.body));
+//     res.status(200).send("set settings");
+// });
+// 
+// app.get('/get-settings', async (req, res) => {
+//     if(typeof(req.session.username) == "undefined") {
+//         res.status(400).send("User not logged in");
+//         return;
+//     }
+//     let key = crypto.createHash('md5').update(req.session.username).digest('hex');
+//     client.get(`settings:${key}`, function (err, reply) {
+//         if(!reply) {
+//             //res.send(JSON.stringify({calendars:[]}));
+//             res.send({calendars:["CRLS", "holidays"]});
+//         } else {
+//             res.send(JSON.parse(reply));
+//         }
+//     });
+// });
+// 
+// app.post('/add-calendar', async (req, res) => {
+//     console.log(req.body.color);
+//     // Security: MUST SANITIZE URLS
+//     if(req.body.id == undefined || !validator.isEmail(req.body.id) ||
+//         req.body.name == undefined || !req.body.name.match(/^[\-0-9a-zA-Z.'' ]+$/g) ||
+//         req.body.color == undefined || !req.body.color.match(/^([\-0-9a-fA-F]){6}$/g)) {
+//         res.status(400).send("Malformed ID, Name, or Color");
+//         return;
+//     }
+// 
+//     // Check to see if it is public and working
+//     const calendar = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(req.body.id)}/events?key=AIzaSyDtbQCoHa4lC4kW4g4YXTyb8f5ayJct2Ao&timeMin=2019-02-23T00%3A00%3A00Z&timeMax=2019-04-08T00%3A00%3A00Z&singleEvents=true&maxResults=9999&_=1552838482460`);
+//     if(!calendar.ok) {
+//         res.status(400).send("Bad Calendar ID");
+//         return;
+//     }
+// 
+//     let cal = JSON.stringify({
+//         name: req.body.name,
+//         id: req.body.id,
+//         color: req.body.color
+//     });
+//     client.lrem('calendars', 0, cal);
+// 
+//     client.rpush('calendars', 
+//     JSON.stringify({
+//         name: req.body.name,
+//         id: req.body.id,
+//         color: req.body.color
+//     }));
+// 
+//     res.status(200).send("added calendar");
+// });
+// 
+// app.get('/get-calendars', async (req, res) => {
+//     client.lrange(`calendars`, 0, -1, function (err, reply) {
+//         for(i in reply) {
+//             reply[i] = JSON.parse(reply[i]);
+//         }
+//         res.send(reply);
+//     });
+// });
 
 io.on('connection', function(socket){
 	console.log('a user connected');
