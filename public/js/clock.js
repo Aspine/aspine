001588a106733d5b664502f001c3dc186350ae09
psvenk <45520974+psvenk@users.noreply.github.com>
2020-03-09 20:37:11 -0400

    Adjust clock for Daylight Savings Time

--- public/js/clock.js
+++ public/js/clock.js
@@ -172,8 +172,8 @@ function redraw_clock() {
     // UTC to EST
     let number = 0;
     let period_name = "";
-    let now = Date.now() - 5 * 60 * 60 * 1000;
-    // let now = Date.now() - 4 * 60 * 60 * 1000;
+    // let now = Date.now() - 5 * 60 * 60 * 1000;
+    let now = Date.now() - 4 * 60 * 60 * 1000;
     let tod = now % (24 * 60 * 60 * 1000);
     if(school_day()) {
         // let tod = 41399000; // Simulate time
