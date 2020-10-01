34a1df11be2cba8a5cd675a186b2b1a91f5f13de
tektaxi <45189395+tektaxi@users.noreply.github.com>
2020-10-01 16:00:42 -0400

    added remember me

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,40 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+// Remember me and autofill functions
+
+function rememberMe() {
+  if(localStorage.getItem("remember") == "yes"){
+  document.getElementById("check").setAttribute('checked', true)
+  }
+  else {
+  document.getElementById("check").setAttribute('checked', false)
+  }
+
+  if (localStorage.getItem("username") === null) {
+  localStorage.removeItem("username")
+  }
+  else {
+  document.getElementById("input-name").setAttribute('value',localStorage.getItem("username"))
+  document.getElementById("input-password").setAttribute('value',localStorage.getItem("password"))
+  }
+
+  if (localStorage.getItem("password") === null){
+  localStorage.removeItem("password")
+  }
+}
+function autofill () {
+  let checkBox = document.getElementById("check");
+      if(checkBox.checked === true) {
+      let name = document.getElementById("input-name").value;
+      let password =  document.getElementById("input-password").value;
+      localStorage.setItem("username", name)
+      localStorage.setItem("password", password)
+      localStorage.setItem("remember", "yes")
+  }
+      if(checkBox.checked === false) {
+          localStorage.removeItem("username")
+          localStorage.removeItem("password")
+          localStorage.setItem("remember", "no")
+          }
+}
