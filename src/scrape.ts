f75a6d57bd0da483937fe2cbca663e56e5777afa
psvenk <45520974+psvenk@users.noreply.github.com>
2020-11-28 11:10:50 -0500

    Introduce error handling + rename functions
    The name "scrape" is a misnomer.

--- src/scrape.ts
+++ src/scrape.ts
@@ -5,18 +5,16 @@ import type { Session, PDFFileInfo, PDFFile, ClassInfo } from "./types";
 // Using `import type` with an enum disallows accessing the enum variants
 import { Quarter } from "./types";
 
-async function scrape_student(
+async function get_student(
   username: string, password: string, quarter: Quarter
 ) {
-  const session = await scrape_login();
-  // Log in to Aspen
-  if (!(await submit_login(username, password, session))) {
-    return { "login_fail": true };
-  }
+  const session = await login(username, password);
   const { student_name, student_oid } = await get_student_info(session);
   const quarter_oid = await get_quarter_oid(session, quarter);
-  const academics = await scrape_academics(session, student_oid, quarter_oid);
-  await logout(session);
+  const academics = await get_academics(session, student_oid, quarter_oid);
+  try {
+    await logout(session);
+  } catch (e) {}
 }
 
 /**
@@ -58,7 +56,7 @@ async function get_quarter_oid(
 /**
  * Get basic information about classes
  */
-async function scrape_academics(
+async function get_academics(
   { session_id }: Session, student_oid: string, quarter_oid: string
 ): Promise<ClassInfo[]> {
   const classes: any[] = await (await fetch(
@@ -84,7 +82,7 @@ async function scrape_academics(
 /**
  * Return an array containing all PDF files
  */
-async function scrape_pdf_files(session: Session): Promise<PDFFile[]> {
+async function get_pdf_files(session: Session): Promise<PDFFile[]> {
   const pdf_files = await list_pdf_files(session);
   return await Promise.all(pdf_files.map(async ({ id, filename }) => ({
     title: filename,
@@ -122,27 +120,21 @@ async function download_pdf(
 }
 
 /**
- * Scrape the Aspen login page and return a session ID and Apache token
- */
-async function scrape_login(): Promise<Session> {
-  const page = await (await fetch(
-    "https://aspen.cpsd.us/aspen/logon.do"
-  )).text();
-  const [, session_id] = /sessionId='(.+)';/.exec(page) || [];
-  const [, apache_token] =
-    /name="org.apache.struts.taglib.html.TOKEN" value="(.+)"/.exec(page) || [];
-  return { session_id, apache_token };
-}
-
-/**
- * Log in to Aspen under a given session using a given username and password.
- * The return value indicates the success of the login.
+ * Log in to Aspen using a given username and password, returning a session.
  * Always make sure to close the session afterwards with logout().
  */
-// TODO use proper error handling instead of returning a boolean
-async function submit_login(
-  username: string, password: string, { session_id, apache_token }: Session
-): Promise<boolean> {
+async function login(username: string, password: string): Promise<Session> {
+  // Get a session from Aspen by visiting the login page
+  const login_page = await (await fetch(
+    "https://aspen.cpsd.us/aspen/logon.do"
+  )).text();
+  const [, session_id] = /sessionId='(.+)';/.exec(login_page) || [];
+  const [, apache_token] =
+    /name="org.apache.struts.taglib.html.TOKEN" value="(.+)"/.exec(
+      login_page
+    ) || [];
+
+  // Submit login username, password, and session information
   const page = await (await fetch("https://aspen.cpsd.us/aspen/logon.do", {
     headers: {
       "Cookie": `JSESSIONID=${session_id}`,
@@ -157,29 +149,40 @@ async function submit_login(
       "password": password,
     }),
   })).text();
-  return !page.includes("Invalid login.");
+
+  if (page.includes("Invalid login.")) {
+    throw new Error("Invalid login");
+  }
+
+  return { session_id, apache_token };
 }
 
 /**
- * Log out of Aspen under a given session.
+ * Log out of Aspen, destroying the given session.
  */
-async function logout({ session_id }: Session): Promise<boolean> {
+async function logout({ session_id }: Session): Promise<void> {
   const page = await (await fetch("https://aspen.cpsd.us/aspen/logout.do", {
     headers: {
       "Cookie": `JSESSIONID=${session_id}`,
     },
     redirect: "manual",
   })).text();
-  return !page.includes("You are not logged on or your session has expired.");
+
+  if (page.includes("You are not logged on or your session has expired.")) {
+    throw new Error("Logout failed");
+  }
 }
 
 // Code for testing purposes
 if (require.main === module) {
   const [username, password] = process.argv.slice(2);
-  scrape_student(username, password, 1).then(console.log);
+  get_student(username, password, 1).then(
+    console.log,
+    e => console.error(`Error: ${e.message}`)
+  );
 }
 
 module.exports = {
-  scrape_student,
-  scrape_pdf_files,
+  get_student,
+  get_pdf_files,
 };
