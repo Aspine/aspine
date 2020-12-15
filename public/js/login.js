9a6ebe0ed929195d054806a6134ee79143d98661
tektaxi <45189395+tektaxi@users.noreply.github.com>
2020-12-15 17:21:22 -0500

    changes remember me to only store username

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,12 @@
-// Remember me - load username/password from localStorage
+// Remember me - load username from localStorage
 window.addEventListener("load", () => {
+  localStorage.removeItem("password");
   if (localStorage.getItem("remember") === "true") {
     document.getElementById("remember-me").setAttribute('checked', true);
   }
   else {
     document.getElementById("remember-me").setAttribute('checked', false);
+    document.getElementById("remember-me").click()
   }
 
   if (localStorage.getItem("username") === null) {
@@ -14,29 +16,19 @@ window.addEventListener("load", () => {
     document.getElementById("input-username").setAttribute(
       'value', localStorage.getItem("username")
     );
-    document.getElementById("input-password").setAttribute(
-      'value', localStorage.getItem("password")
-    );
-  }
-
-  if (localStorage.getItem("password") === null) {
-    localStorage.removeItem("password");
   }
 });
 
-// Remember me - store username/password in localStorage
+// Remember me - store username in localStorage
 document.getElementById("login100-form-btn").addEventListener("click", () => {
   const checkBox = document.getElementById("remember-me");
   if (checkBox.checked) {
     const username = document.getElementById("input-username").value;
-    const password = document.getElementById("input-password").value;
     localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
     localStorage.setItem("remember", "true");
   }
   else {
     localStorage.removeItem("username");
-    localStorage.removeItem("password");
     localStorage.setItem("remember", "false");
   }
 });
