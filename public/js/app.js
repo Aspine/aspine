98b08305cb223aeba82f6c73b3e87478ea532025
psvenk <45520974+psvenk@users.noreply.github.com>
2020-12-26 15:14:02 -0500

    Disable service worker code (for now)
    Currently sw.js does nothing except for throw errors in the console.
    While it is certainly a laudable goal to make Aspine a true PWA, this
    should be done separately and we do not need a stub throwing errors
    until we do that.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,4 @@
+/*
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(registration => {
@@ -9,3 +10,4 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+*/
