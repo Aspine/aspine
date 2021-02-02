02b5a5173b9965cd54f28c2c1747826eca136302
notrodes <reallystupidid@protonmail.com>
2021-02-02 11:56:10 -0500

    add tooling and some doc for script

--- gen-key.sh
+++ gen-key.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #https://www.rosehosting.com/blog/how-to-generate-a-self-signed-ssl-certificate-on-linux/
+# Generates self signed cert for dev purposes
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out local.crt -keyout local.key
 openssl rsa -passin pass:x -in keypair.key -out local.key
 openssl req -new -key local.key -out local.csr
