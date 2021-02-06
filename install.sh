8216e715cfc7b9697167cdf9dc9799a77bb0ca65
notrodes <46428026+notrodes@users.noreply.github.com>
2021-02-06 14:33:38 -0500

    Remove install scripts (#251)
    Remove now-unnecessary install scripts, and remove references to them
    from README.md
    
    Co-authored-by: psvenk <45520974+psvenk@users.noreply.github.com>

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew install node
-brew install redis
-
-npm install
--- npminstall.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-npm install
-echo.
-echo To run Aspine, double-click on the file "start.bat".
-echo Press any key to close this window.
-pause
