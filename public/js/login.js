d49671a2d7e0a584f60c4c23fbb8a6d82690a6aa
psvenk <45520974+psvenk@users.noreply.github.com>
2021-03-18 12:38:13 -0400

    Extend session with "remember me"
    Instead of just storing the username in client-side storage and
    populating it on the login page, keep the user logged in for up to 30
    days if they select "remember me".

--- public/js/login.js
+++ public/js/login.js
@@ -16,38 +16,3 @@ if ((err = (new URLSearchParams(window.location.search)).get("error"))) {
     elem.textContent = message;
   }
 }
-
-// Remember me - load username from localStorage
-window.addEventListener("load", () => {
-  localStorage.removeItem("password");
-  if (localStorage.getItem("remember") === "true") {
-    document.getElementById("remember-me").setAttribute('checked', true);
-  }
-  else {
-    document.getElementById("remember-me").setAttribute('checked', false);
-    document.getElementById("remember-me").click();
-  }
-
-  if (localStorage.getItem("username") === null) {
-    localStorage.removeItem("username");
-  }
-  else {
-    document.getElementById("input-username").setAttribute(
-      'value', localStorage.getItem("username")
-    );
-  }
-});
-
-// Remember me - store username in localStorage
-document.getElementById("login100-form-btn").addEventListener("click", () => {
-  const checkBox = document.getElementById("remember-me");
-  if (checkBox.checked) {
-    const username = document.getElementById("input-username").value;
-    localStorage.setItem("username", username);
-    localStorage.setItem("remember", "true");
-  }
-  else {
-    localStorage.removeItem("username");
-    localStorage.setItem("remember", "false");
-  }
-});
